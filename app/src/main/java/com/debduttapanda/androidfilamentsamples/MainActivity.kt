7ad3bb8774319cfafb82053e1bba9df7a532a9d9
Debdutta Panda <bedmad82@gmail.com>
2022-12-31 18:43:05 +0530

    Initial commit

---
+++ app/src/main/java/com/debduttapanda/androidfilamentsamples/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.debduttapanda.androidfilamentsamples
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.bt_hello_triangle).setOnClickListener {
+            startActivity(Intent(this,HelloTriangle::class.java))
+        }
+    }
+}
